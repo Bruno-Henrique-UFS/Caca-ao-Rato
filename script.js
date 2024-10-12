ef1711252fe150d5ad2977139ef7578333a3f304
raissacavalcante <raissa.cavalcante@dcomp.ufs.br>
2024-10-12 13:59:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,36 @@ document.addEventListener("DOMContentLoaded", function () {
         intervalorato: null,
     };
 
+    function removerRatos(index) {
+    if (index >= buracos.length) return;
+    buracos[index].classList.remove('rato');
+    buracos[index].removeEventListener('click', lidarComCliqueRato);
+    removerRatos(index + 1);
+}
+//essa função percorre o Array buracos, removendo o rato de cada buraco e tirando o ouvinte de
+//eventos de cada um, deixando todos os buracos vazios no começo do jogo
+
+
+
+
+
+
+
     
+function FinalizarJogo() {
+    clearInterval(estadoDoJogo.contagemRegressiva);
+    clearInterval(estadoDoJogo.intervalorato);
+    estadoDoJogo.jogoAcabado = true;
+    alert(`Jogo Finalizado!\nSua pontuação final: ${estadoDoJogo.pontuacao}`);
+    estadoDoJogo.pontuacao = 0;
+    estadoDoJogo.timer = 60;
+    displayPontuacao.textContent = `Pontuação: ${estadoDoJogo.pontuacao}`;
+    displayTimer.textContent = `Tempo: ${estadoDoJogo.timer}s`;
+    botaoIniciar.disabled = false;
+    botaoFinalizar.disabled = true;
+    clicado.style.fill = "black";
+}
+//função que finaliza o jogo, trazendo as informações de pontuação finais e habilita para ser jogado novamente
 
     botaoIniciar.addEventListener("click", iniciarJogo);
     botaoFinalizar.addEventListener("click", finalizarJogo);
